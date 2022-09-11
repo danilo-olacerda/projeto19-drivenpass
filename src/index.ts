d1c645006ee03501813cc1caefd24004fd11a568
Danilo <dandam3006@gmail.com>
2022-09-11 14:31:46 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routes/router';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
